72131dd27e742ce8abde050b56d7768f9f3937c1
swapnilnakate7 <nakate.swapnil7@gmail.com>
2024-07-31 00:50:02 +0530

    updated versions

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { SnButtonComponent } from '@libs/sn-button-x/src/public-api';
-// import  {SnCardComponent}from '@libs/sn-card/src/public-api';
-// import  {SnInputComponent}from '@libs/sn-input/src/public-api';
+import { SnButtonComponent } from '@libs/sn-button-x/src/public-api';
+import  {SnCardComponent}from '@libs/sn-card/src/public-api';
+import  {SnInputComponent}from '@libs/sn-input/src/public-api';
 
-import { SnButtonComponent } from '@dist/sn-button-x';
-import { SnCardComponent } from '@dist/sn-card';
-import { SnInputComponent } from '@dist/sn-input';
+// import { SnButtonComponent } from '@dist/sn-button-x';
+// import { SnCardComponent } from '@dist/sn-card';
+// import { SnInputComponent } from '@dist/sn-input';
 
 @Component({
   selector: 'sn-list',
