999dbbcdf92aaea0ddc235e758781d640e3bb080
swapnilnakate7 <nakate.swapnil7@gmail.com>
2026-03-03 11:00:05 +0530

    fix: update storybook config to scan projects directory

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,9 @@ import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   "stories": [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../projects/**/*.mdx",
+    "../projects/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-a11y",
